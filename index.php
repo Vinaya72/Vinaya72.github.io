1cf8d425b36e8d943411f6c16718abb5dc79ce3e
Vinaya72 <89534162+Vinaya72@users.noreply.github.com>
2021-08-26 16:44:53 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,208 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"/>
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <title>Monali</title>
+    
+</head>
+<body>
+    
+    <header class="l-header">
+        <nav class="nav bd-grid">
+            <div>
+                <a href="#" class="nav__logo">Monali</a>
+            </div>
+
+            <div class="nav__menu" id="nav-menu">
+                <ul class="nav__list">
+                    <li class="nav__item"><a href="#home" class="nav__link active">Home</a></li>
+                    <li class="nav__item"><a href="#about" class="nav__link">About</a></li>
+                    <li class="nav__item"><a href="#skills" class="nav__link">Skills</a></li>
+                    <li class="nav__item"><a href="#work experience" class="nav__link">Work Experience</a></li>
+                    <li class="nav__item"><a href="#blog" class="nav__link">Blog</a></li>
+                    
+                </ul>
+            </div>
+
+            <div class="nav__toggle" id="nav-toggle">
+                <i class="fas fa-bars"></i>
+            </div>
+        </nav>
+    </header>
+
+    <main class="l.main">
+        <section class="home" id="home">
+              <div class="home__container bd-grid">
+                  <h1 class="home__title">M<i class="far fa-smile-beam"></i>NA<br>LI.</h1>
+
+                    <div class="home__scroll">
+                        <a href="#about" class="home__scroll-link">Scroll Down <i class="fas fa-arrow-down"></i></a>
+                    </div>
+                  
+              </div>
+        </section>
+
+        <section class="about section" id="about">
+              <h2 class="section-title">About Me</h2>
+
+              <div class="about__container bd-grid">
+                <div class="about__img">
+                    <img style="border-radius: 175px 100px 175px 100px ;" src="assets/img/mons1.jpg" alt="">
+                </div>
+
+                  <div>
+                      <h2 class="about__subtitle">I am Monali Baskar</h2>
+                      <span class="about__profession">Student</span>
+                      <p class="about__text">An energetic and outgoing individual aiming to become an
+                          "Influential Global Citizen" in the field of Digital Marketing, Branding, and Stratification.
+                          I am constantly upskilling and trying to build a profile for myself!
+                      </p>
+                  </div>
+              </div>
+        </section>
+
+        <section class="skills section" id="skills">
+             <h2 class="section-title">Skills</h2>
+
+             <div class="skills__container bd-grid">
+                 <div class="skills__box">
+                     <h3 class="skills__subtitle">Social Media Managemnet</h3>
+                     <h4 class="skills__name">M.s Office and Canva</h4>
+                     <h4 class="skills__name">Content Creation and Research</h4>
+                     <h4 class="skills__name">Google Analytics</h4>
+                     <h4 class="skills__name">Leadership and Adaptability</h4>
+                     <h4 class="skills__name">Communication and Interpersonal</h4>
+                 </div>
+
+                 <div class="skills_img">
+                    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                        <ol class="carousel-indicators">
+                            <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"></li>
+                            <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
+                            <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
+                        </ol>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img class="d-block w-100" src="assets/img/analytics.png" alt="First slide">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="assets/img/content.jpeg" alt="Second slide">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="assets/img/light.png" alt="Third slide">
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </a>
+                    </div>
+                 </div>
+             </div>
+        </section>
+
+        <section class="section" id="work experience">
+            <h2 class="section-title">Work Experience</h2>
+            <div class="container">
+                <div class="timeline">
+                    <ul>
+                        <li>
+                            <div class="timeline-content">
+                                <h2 class="date">June 2021 - Current</h2>
+                                <h1>CREATIVE GEOMETRY - INTERIOR DESIGNS</h1>
+                                <p>Social Media Intern | Create Story Engagements on a daily basis |
+                                    Content Generation & ideation for Social media.
+                                </p>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="timeline-content">
+                                <h2 class="date">Sept 2020 - April 2021</h2>
+                                <h1>NATIVE CREATIV COMMUNICATIONS PVT. LTD.</h1>
+                                <p>Social Media Intern | Creating and maintaining a database of leads 
+                                    for a school | Research and Data collection for brand guidelines.
+                                </p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+    
+
+        </section>
+        
+    <section class="blog section" id="blog">
+            <h2 class="section-title">Blogs</h2>
+
+            <div class="blog__container bd-grid">
+                <div class="card h-100 zoom">
+                    <div class="card-body">
+                      <img class="card-img-top"  src="assets/img/blog1.jpeg" alt=blog1 />
+                    </div>
+                      <h4 class="card-title text-white text-center">Book Review</h4>
+                      <p class="card-text text-white text-center">Who moved my cheese?</p>
+                      <a href="https://link.medium.com/U1xCk4jX0ib" class="stretched-link"></a>
+                </div>
+                <div class="card h-100 zoom">
+                    <div class="card-body">
+                      <img class="card-img-top"  src="assets/img/mentalhealth2.jpeg" alt=blog2 />
+                    </div>
+                      <h4 class="card-title text-white text-center">Mental Health</h4>
+                      <p class="card-text text-white text-center">My thoughts on having positive mindset..</p>
+                      <a href="https://link.medium.com/WERBNlnX0ib" class="stretched-link"></a>
+                </div>
+                <!-- <div class="card h-100 zoom">
+                    <div class="card-body">
+                      <img class="card-img-top"  src="assets/img/blog3.jpeg" alt=blog1 />
+                    </div>
+                      <h4 class="card-title text-white text-center">Mental Health</h4>
+                      <p class="card-text text-white text-center">Keeping your mental health healthy is a must</p>
+                      <a href="#" class="stretched-link"></a>
+                </div> -->
+            </div>
+            <h4 class="g-link my-5 py-5"><a href="https://medium.com/@monalibaskar">More Blogs <i class="far fa-hand-peace"></i></a></h4>
+        </section>
+    </main>
+
+    <footer class="footer section">
+        <div class="footer__container bd-grid">
+              <div class="footer__data">
+                  <h2 class="footer__title">Thanks for the time.</h2>
+
+              </div>
+
+              <div class="footer__data">
+                  <h2 class="footer__title">Explore</h2>
+
+                  <ul>
+                      <li><a href="#home" class="footer__link">Home</a></li>
+                      <li><a href="#about" class="footer__link">About</a></li>
+                      <li><a href="#skills" class="footer__link">Skills</a></li>
+                      <li><a href="#work experience" class="footer__link">Work Experience</a></li>
+                      <li><a href="#blog" class="footer__link">Blog</a></li>
+                  </ul>
+              </div>
+
+              <div class="footer__data">
+                  <h2 class="footer__title">Follow</h2>
+                   <a href="https://www.linkedin.com/in/monali-baskar-456322200" class="footer__social text-center"><i class="fab fa-linkedin"></i></a>
+                   
+              </div>
+        </div>
+    </footer>
+ 
+
+    
+    <script src="assets/js/main.js"></script> 
+</body>
+</html>
